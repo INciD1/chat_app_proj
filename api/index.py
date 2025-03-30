423f0e9661bee06fda2564be0293a0134644ec00
INciDious1 <166299784+INciD1@users.noreply.github.com>
2025-03-31 00:08:11 +0700

    Delete api directory

--- api/index.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from main import app
-
-# This route handler is required for Vercel serverless functions
-def handler(request):
-    return app(request['environ'], request['start_response'])
