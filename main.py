98958d892be4aa192832b7a6c1b10cb8bdfb8af7
INciDious1 <166299784+INciD1@users.noreply.github.com>
2025-03-31 00:01:52 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "thisisasecretkey")
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='polling')
 
 rooms = {}
 
