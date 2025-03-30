992b29453c85cfb2e4544b471b4ad88d1891deb4
INciDious1 <166299784+INciD1@users.noreply.github.com>
2025-03-30 23:58:25 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,6 @@
-from http.server import BaseHTTPRequestHandler
+from flask import Flask
 from main import app
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, Vercel!'.encode())
-        return
+# This route handler is required for Vercel serverless functions
+def handler(request):
+    return app(request['environ'], request['start_response'])
